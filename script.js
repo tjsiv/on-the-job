afa5af300875d0d54fbfb8e97b3b22a360847aaf
tjsiv <tjsivelsjr@gmail.com>
2024-01-30 21:11:03 -0800

    Added CSS and JS files

---
+++ script.js
@@ -0,0 +1,14 @@
+// include all section ids as variables
+var logo = $("#storeLogo") //set to fade in
+var aboutSection = $("#infoAbout")
+var favor = $("#mathFav") //set to fade in
+var qtrMen = $("#qtrMenu") //set to fade in
+var strLocal = $("#storeLocation")//set to fade in
+var hours = $(".hoursTbl")//set to fade in
+
+//all fade ins above need 300ms delay & opacity 1-600
+logo.fadeIn(3000)
+favor.fadeIn(3000)
+qtrMen.fadeIn(3000)
+strLocal.fadeIn(3000)
+hours.fadeIn(3000)
